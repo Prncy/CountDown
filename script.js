f007208c49dbf61462b74b8ce99e18ea6f9e6c11
Prncy <princelay1313@gmail.com>
2026-09-12 13:50:56 +0200

    Update target reveal date in countdown script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Reveal Countdown Constants Configuration
-    const targetRevealDate = new Date("2026-09-12T13:47:00").getTime();
+    const targetRevealDate = new Date("2026-09-12T14:10:00").getTime();
     
     // Element Anchor Hooks
     const countdownContainer = document.getElementById("countdown");
